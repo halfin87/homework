80f428c3f3087baf599c983cbe060b67eb1986cd
halfin rodion <halfin87@mail.ru>
2023-02-24 18:07:43 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(" Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+  Console.WriteLine(" Первое число больше второго");
+}
+else Console.WriteLine(" Второе число больше первого ");
